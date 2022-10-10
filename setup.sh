e3f13bf526377189e7ebb70f2f51b08639276d97
Jarkko Leponiemi <jarkko.leponiemi@eduix.fi>
2022-10-10 14:49:53 +0300

    Version 4.4.3

--- setup.sh
+++ setup.sh
@@ -10,7 +10,6 @@ apt-get update && \
     apt-get -y dist-upgrade && \
     apt-get install -y \
         ssl-cert\
-        libmariadb-java\
     && apt-get -y autoremove \
     && apt-get autoclean
 
@@ -31,7 +30,6 @@ if [[ "${CROWD_SHA256_CHECKSUM}" != "$(sha256sum /opt/crowd.tar.gz | cut -d' ' -
 fi
 tar -xzf /opt/crowd.tar.gz --directory "${CROWD_INSTALL}" --strip-components=1 --no-same-owner \
     && rm -f /opt/crowd.tar.gz \
-    && ln -s /usr/share/java/mariadb-java-client.jar /opt/atlassian/crowd/apache-tomcat/lib/mariadb-java-client.jar \
     && chown -R ${RUN_USER}:${RUN_GROUP}     "${CROWD_INSTALL}/apache-tomcat/bin" \
     && chown -R ${RUN_USER}:${RUN_GROUP}     "${CROWD_INSTALL}/apache-tomcat/work" \
     && chown -R ${RUN_USER}:${RUN_GROUP}     "${CROWD_INSTALL}/apache-tomcat/temp" \
